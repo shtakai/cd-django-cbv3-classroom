b01bc51b76873ffdbfc7db85105c772ca1b54e7a
Alyson Takai <shtakai@gmail.com>
2016-05-31 17:09:10 -0700

    created models

--- apps/classroom/models.py
+++ apps/classroom/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Teacher(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    subject = models.ForeignKey('Subject')
+
+    class Meta:
+        db_table = 'teachers'
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    teacher = models.ForeignKey('Teacher')
+
+    class Meta:
+        db_table = 'students'
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
